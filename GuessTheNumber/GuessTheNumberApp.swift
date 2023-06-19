d15a825e679410371ffe6c66854818db85f72ae3
polosa00 <i@apolochanin.ru>
2023-06-19 22:39:22 +0300

    Initial Commit

---
+++ GuessTheNumber/GuessTheNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheNumberApp.swift
+//  GuessTheNumber
+//
+//  Created by Александр Полочанин on 19.06.23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
